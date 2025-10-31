522c1884a0736b3f2eb6b27e969c753351300bf4
juanpiyoo11 <juan.pablo.yoo@gmail.com>
2025-10-30 23:33:45 -0300

    testing

--- src/main/java/ar/edu/uade/toto/toto_backend/service/NluService.java
+++ src/main/java/ar/edu/uade/toto/toto_backend/service/NluService.java
@@ -317,8 +317,8 @@ public class NluService {
             ObjectNode root = mapper.createObjectNode();
             root.put("model", model);
             root.put("temperature", 0.0);
-            root.put("stream", false);
-            root.put("max_output_tokens", 80);
+            root.put("stream", true);
+//            root.put("max_output_tokens", 80);
 
             ArrayNode input = root.putArray("input");
             input.add(objectMsg("system", systemPrompt));
--- src/main/java/ar/edu/uade/toto/toto_backend/service/OpenAIPromptService.java
+++ src/main/java/ar/edu/uade/toto/toto_backend/service/OpenAIPromptService.java
@@ -58,8 +58,7 @@ public class OpenAIPromptService {
         ObjectNode root = mapper.createObjectNode();
         root.put("model", model);
         root.put("temperature", 0.7);
-        root.put("stream", false);
-        root.put("max_output_tokens", 80);
+        root.put("stream", true);
         root.set("input", buildInput(prompt));
 
         Request request = new Request.Builder()
